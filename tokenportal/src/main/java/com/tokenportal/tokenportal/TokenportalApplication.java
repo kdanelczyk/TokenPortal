386e09fec7109d708b9bf4b6a7b01eacfb89d77d
Kamil <kamil.danelczyk@gmail.com>
2024-08-08 19:37:44 +0200

    Add project with Dependencies

---
+++ tokenportal/src/main/java/com/tokenportal/tokenportal/TokenportalApplication.java
@@ -0,0 +1,13 @@
+package com.tokenportal.tokenportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenportalApplication.class, args);
+	}
+
+}
---
+++ tokenportal/src/test/java/com/tokenportal/tokenportal/TokenportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokenportal.tokenportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
